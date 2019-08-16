305a14b9f462c6820c1146b5c746cedc77636639
alphonse37 <jesus.colocho.37@gmail.com>
2019-08-16 17:23:50 -0600

    slider funciona C:

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
-Vue.use(VueAgile)
 
 app = new Vue ({
     el: '#promociones',
-    components: {
-		agile: VueAgile,
-    },
-    
     data:  {
            slides: [],
-           currentSlide: ""
     },
 
-
     mounted() {
-        this.slides[this.currentSlide]
         axios.get("https://pmpu.000webhostapp.com/api/promocion")
         .then(res => {
             this.slides = res.data;
